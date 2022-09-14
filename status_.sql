e00e29ae22ef5865e062a94e1fcd66e99dbf85c2
Ekaterina_Kostyra <60934863+EkaterinaKostyra@users.noreply.github.com>
2022-09-14 15:36:05 +0300

    Delete status_.sql

--- status_.sql
+++
@@ -1,3 +0,0 @@
-create table status_ (IDstatus_ int, IDcustomer int, status_name text, status_level int,
-primary key (IDstatus_, IDcustomer),
-constraint fr8 foreign key (IDcustomer) references customer(IDcustomer));
