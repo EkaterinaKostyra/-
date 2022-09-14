c9bb8ade8171cb32e1b9d04928213158e392fd07
Ekaterina_Kostyra <60934863+EkaterinaKostyra@users.noreply.github.com>
2022-09-14 15:32:12 +0300

    Delete 2.sql

--- 2.sql
+++
@@ -1,9 +0,0 @@
-SELECT COUNT(distinct blabla.IDcustomer) FROM (
-    SELECT
-        IDcustomer,
-        firstname,
-        lastname,
-        COUNT(distinct IDclub) as clubs_number
-    FROM fitness_clubs
-    GROUP BY IDcustomer
-    ) as blabla;
